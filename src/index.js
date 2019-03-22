a0b6664de05d9318d2a00dba783a5ee4b8bdb6ff
petriken <petriken82@gmail.com>
2019-03-22 17:04:45 +0300

    feat: start solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  for (i=0; i < 9; i++) {
+    for (j=0; j < 9; j++) {
+    const number = [1,2,3,4,5,6,7,8,9];
+    } 
+  }
+  
+  return resultMatrix
 }
